4f018d274b81865679610980560308979536b229
hamzasashiro <hamzamassir01@gmail.com>
2023-03-16 14:18:43 -0700

    Tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
-* main - Prints out a string to stdout.
-* Return : 0 (Succes)
-*/
-int main(void) {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+ * main - puts
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
-* main - Prints out a string to stdout.
-* Return : 0 (Succes)
-*/
-int main(void) {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(c));
-printf("Size of a clong long int: %lu byte(s) \n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
-return (0);
+ * main - puts
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
